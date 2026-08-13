dcb704277c0081ae7eb52e5dc46658e40ce7fa24
Gabriel Nascimento <gabrielpiresnasc@gmail.com>
2026-08-13 09:53:22 -0300

    Atualizacao da tela com Texto centralizado,porem necessita de reajuster pois qualquer objeto colocado na pagina sera centralizado

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ def main(page: ft.Page):
     
     page.title = "Meu Primeiro App"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
 
     texto = ft.Text(value="Olá, Mundo!", size=30)
 
     #add o texto na pagina
-    page.add(ft.Column([texto], alignment=ft.MainAxisAlignment.CENTER))
+    page.add(ft.Column([texto]))
 
 ft.app(target=main)

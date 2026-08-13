8818a998affdd318c1b0efced8fa24b7bb9b551a
Gabriel Nascimento <gabrielpiresnasc@gmail.com>
2026-08-13 09:57:07 -0300

    Atualizado de column para container,porem erro que expand = true ocupa todo espaco e a tela fica branca ,como se tivesse ocupando todo espaco

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ import flet as ft
 def main(page: ft.Page):
     
     page.title = "Meu Primeiro App"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+
+    page.vertical_alignment = ft.MainAxisAlignment.START    
+    page.horizontal_alignment = ft.CrossAxisAlignment.START
 
 
     texto = ft.Text(value="Olá, Mundo!", size=30)
 
+    ContainerText = ft.Container(
+        content=texto,
+        alignment="center",
+        expand=True
+    )
+
     #add o texto na pagina
-    page.add(ft.Column([texto]))
+    page.add(ContainerText)
 
 ft.app(target=main)

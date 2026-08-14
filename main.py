d6c2d4db2c6a945e9583e4a2b763e000ec794698
Gabriel Nascimento <gabrielpiresnasc@gmail.com>
2026-08-14 09:14:35 -0300

    Add .env,.gitignore,pasta de imagens futuras e,inicializacao da funcao para criacao de login

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.START
 
     texto = ft.Text(value="Bem-vindo a Nesvt!", size=30)
-    
+
+
+    def login_click(e):
+            #Criar funcao para inserir banco de dados 
+            page.add(ft.Text(value="Login realizado com sucesso!", size=20, color=ft.Colors.GREEN))
+
+
     #container da area de login
     container_login = ft.Container(
         #criacao de um content / column dentro do container 
-        
         content=ft.Column(
             controls=[
                 texto,  
                 ft.TextField(label="Usuário"),
                 ft.TextField(label="Senha", password=True, can_reveal_password=True),
-                ft.Button("Entrar", on_click=lambda e: print("Entrou!"))
+                ft.Button("Entrar", on_click=login_click)
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
@@ -27,6 +32,8 @@ def main(page: ft.Page):
         alignment=  ft.Alignment.CENTER ,
         expand=True
     )
+    
+    
 
     page.add(container_login)
 
